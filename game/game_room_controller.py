a3c9682b193c7010071d0fa348fc250823e8bc53
Adis Azhar <you@example.com>
2019-12-01 01:14:22 +0700

    game room server progress

--- game/game_room_controller.py
+++ game/game_room_controller.py
@@ -1,6 +1,10 @@
 import Pyro4
 from threading import Lock, Thread
 
+# TODO:
+# 1. contact main server once initialized
+# 2. propagate data to connected users
+
 
 @Pyro4.expose
 class GameRoomController:
@@ -8,22 +12,26 @@ class GameRoomController:
         self.game_room_name = identity
         self.players = []
         self.spectators = []
-        # Init tic tac toe positions
-        self.game_positions = [''] * 9
-        self.max_players = 2
 
+        # init types
         self.TYPE_SPECTATOR = 'spectator'
         self.TYPE_PLAYER = 'player'
 
         self.TYPE_PLAYER_X = 'player:x'
         self.TYPE_PLAYER_O = 'player:o'
 
+        # Init tic tac toe positions
+        self.game_positions = [None] * 9
+        self.max_players = 2
+
+        self.player_turn = self.TYPE_PLAYER_X
+
         self.lock = Lock()
 
         print("Game controller is initialised!")
 
     def connect(self, participant, join_type):
-        player_type = ''
+        player_type = None
 
         self.lock.acquire()
 
@@ -33,7 +41,7 @@ class GameRoomController:
             return {
                 'status': 'ok',
                 'message': message,
-                'game_info': player_type
+                'data': player_type
             }
         # Participant will become spectators when the players amount is fulfilled
         elif join_type == self.TYPE_SPECTATOR or len(self.players) >= self.max_players:
@@ -42,6 +50,7 @@ class GameRoomController:
                 'info': participant
             }
             self.spectators.append(spec)
+            player_type = 'spectator:{}'.format(len(self.spectators))
             message = 'joined as spectator'
         elif join_type == self.TYPE_PLAYER:
             if len(self.players):
@@ -57,23 +66,50 @@ class GameRoomController:
 
         self.lock.release()
 
-        print(self.players)
-        print(self.spectators)
+        print('players: ', self.players)
+        print('spectators: ', self.spectators)
 
         return {
             'status': 'ok',
             'message': message,
-            'game_info': player_type
+            'data': player_type
         }
 
     def make_a_move(self, location, player_type):
+        self.lock.acquire()
         self.game_positions[location] = player_type
+        response = self.check_winner()
+        self.lock.release()
+        print('game positions: ', self.game_positions)
+
+        return {
+            'status': 'ok',
+            'message': 'successfully moved',
+            'data': {
+                'winner': response,
+                'positions': self.game_positions
+            }
+        }
+
+    def check_winner(self):
+        # Todo return which player won
+        # Check horizontal
+        if self.game_positions[0] == self.game_positions[1] == self.game_positions[2] is not None \
+                or self.game_positions[3] == self.game_positions[4] == self.game_positions[5] is not None \
+                or  self.game_positions[6] == self.game_positions[7] == self.game_positions[8] is not None:
+            return "a winner is found"
+        # Check vertical
+        elif self.game_positions[0] == self.game_positions[3] == self.game_positions[6] is not None \
+                or self.game_positions[1] == self.game_positions[4] == self.game_positions[7] is not None \
+                or self.game_positions[2] == self.game_positions[5] == self.game_positions[8] is not None:
+            return "a winner is found"
+        # Check horizontal
+        elif self.game_positions[0] == self.game_positions[4] == self.game_positions[8] is not None \
+                or self.game_positions[2] == self.game_positions[4] == self.game_positions[6] is not None:
+            return "a winner is found"
+        return None
 
     def ping(self):
         return {
             'status': 'ok'
         }
-
-
-if __name__ == '__main__':
-    grc = GameRoomController('identity123')
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 1. Set venv using python 3
 2. Run command `sudo apt-get install python3.6-tk`
-3. Start coding!
+3. Run command `pyro4-ns -n localhost -p 1337`
+4. Start coding!
